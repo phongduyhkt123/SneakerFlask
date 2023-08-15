26b49f8086b436f058056e48d21cb08da0922fab
phongduyhkt123 <76527901+phongduyhkt123@users.noreply.github.com>
2023-08-15 17:20:13 +0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 # app/routes.py
 import json
+import os
 from flask import render_template,  request, make_response
 from .config.colors import YELLOW
 from app import app
 
 @app.route('/')
 def product_page():
-    with open('app/data/product.json') as f:
+    my_dir = os.path.dirname(__file__)
+    json_file_path = os.path.join(my_dir, 'data/product.json')
+    with open(json_file_path) as f:
         products_data = json.load(f)
 
     cart_items = request.cookies.get('cartItems')
